ba27e814a3852b966f1779f914b9901a93ed3c75
Azmine Toushik Wasi <azmine.wasi@gmail.com>
2021-04-17 02:49:58 +0600

    Updated

--- Sending  SMS Tuilio/code.py
+++ Sending  SMS Tuilio/code.py
@@ -1,4 +1,4 @@
 from twilio.rest import Client
-client=Client("ACb67bda58d52aab52bc50cc80f6984873","6f25a2eda542fa05c56af71836684155")
-client.messages.create(to=("+8801521526792"),
-	from_="+16124414655",body="this is send from wasi twilio")
+client=Client("","")
+client.messages.create(to=(""),
+	from_="",body="this is send from wasi twilio")
