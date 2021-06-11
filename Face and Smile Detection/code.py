8b8948e7c4ef9b6eba6235806dc8087be07adfde
Azmine Toushik Wasi <azmine.wasi@gmail.com>
2021-06-11 15:23:11 +0600

    Face detector

--- Face and Smile Detection/code.py
+++ Face and Smile Detection/code.py
@@ -1,7 +1,8 @@
 import cv2
 
-face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-smile_cascade=cv2.CascadeClassifier('harcascade_smile.xml')
+face_cascade=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_smile.xml')
 
 def detect(gray, frame):
 	faces=face_cascade.detectMultiScale(gray,1.3,5)
@@ -9,19 +10,22 @@ def detect(gray, frame):
 		cv2.rectangle(frame,(x,y),((x+w),(y+h)),(255,0,0),2)
 		roi_gray=gray[y:y + h,x:x+w]
 		roi_color=frame[y:y + h,x:x+w]
-		smiles=smile_cascade.detectMultiScale(roi_gray,1.8,20)
-
-		for (sx,sy,sw,sh) in smiles:
-			cv2.rectangle(roi_color,(sx,sy),((sx+sw),(sy+sh)),(0,0,225),2)
+		facesy=eye_cascade.detectMultiScale(roi_gray,1.3,5)
+		for (x,y,w,h) in facesy:
+			cv2.rectangle(roi_color,(x,y),((x+w),(y+h)),(255,90,180),2)
+		facesyT=smile_cascade.detectMultiScale(roi_gray,1.3,5)
+		for (x,y,w,h) in facesyT:
+			cv2.rectangle(roi_color,(x,y),((x+w),(y+h)),(25,190,190),2)
 	return frame
 
 video_capture=cv2.VideoCapture(0)
 while True:
-	_,frame=video_capture.read()
-	gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	ret,frame=video_capture.read()
+	gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	canvas=detect(gray,frame)
 	cv2.imshow('Video',canvas)
-	if cv2.waitKey(1) & 0xff==ord('q'):
-		breal
+	k = cv2.waitKey(30) & 0xff  
+	if k==27:
+		break  
 video_capture.release()
 cv2.destroyAllWindows()
